2635a8ef19548bdec79ea7f46399c813a8976020
Nate West <natescott.west@gmail.com>
2014-02-22 09:05:48 -0600

    Add card name endpoint
    Plus a bit of cleanup

--- lib/mtgapi/client.rb
+++ lib/mtgapi/client.rb
@@ -17,15 +17,24 @@ module Mtgapi
     end
 
     def find_by(attr, value)
-      self.send("#{attr.to_s}_url", value)
+      url = "#{self.send("#{attr.to_s}_url", value)}#{token_append}"
+      self.class.get(url)
     end
 
     def id_url(id)
-      "id/#{id}?token=#{token}"
+      "card/id/#{id}"
+    end
+
+    def name_url(name)
+      "card/name/#{name}"
     end
 
     def search_url(search_string)
-      "/search/#{search_string}?token=#{token}"
+      "/search/#{search_string}"
+    end
+
+    def token_append
+      "?token=#{token}"
     end
   end
 end
--- test/client_test.rb
+++ test/client_test.rb
@@ -7,23 +7,30 @@ class ClientTest < Minitest::Test
     @client = Mtgapi::Client.new("testtoken")
   end
 
+  def do_return
+    ->(arg) { return arg }
+  end
+
   def test_id_url
-    assert_equal "id/1?token=#{client.token}", client.id_url(1)
+    assert_equal "card/id/1", client.id_url(1)
+  end
+
+  def test_name_url
+    assert_equal "card/name/test", client.name_url("test")
   end
 
   def test_search_url
-    assert_equal "/search/test?token=#{client.token}", client.search_url("test")
+    assert_equal "/search/test", client.search_url("test")
   end
 
   def test_find_by
-    client.stub(:id_url, true) do
-      assert client.find_by(:id, 1)
+    Mtgapi::Client.stub(:get, do_return) do
+      assert_equal "card/id/1?token=#{client.token}", client.find_by(:id, 1)
     end
-    assert_equal "id/1?token=#{client.token}", client.find_by(:id, 1)
   end
 
   def test_search
-    Mtgapi::Client.stub(:get, ->(arg) { return arg }) do
+    Mtgapi::Client.stub(:get, do_return) do
       assert_equal client.search_url("test"), client.search("test")
     end
   end
