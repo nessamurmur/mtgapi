254ca19efc0d14fdecead9d81b2c36f1fee11c54
Nate West <natescott.west@gmail.com>
2014-02-22 08:47:49 -0600

    Mtgapi::Client#find_by

--- lib/mtgapi/client.rb
+++ lib/mtgapi/client.rb
@@ -6,12 +6,26 @@ module Mtgapi
 
     base_uri "http://mtgapi.com/api/v1/fetch"
 
+    attr_reader :token
+
     def initialize(token = ENV['MTG_TOKEN'])
       @token = token
     end
 
     def search(search_string)
-      self.class.get("/search/#{search_string}?token=#{@token}")
+      self.class.get(search_url(search_string))
+    end
+
+    def find_by(attr, value)
+      self.send("#{attr.to_s}_url", value)
+    end
+
+    def id_url(id)
+      "id/#{id}?token=#{token}"
+    end
+
+    def search_url(search_string)
+      "/search/#{search_string}?token=#{token}"
     end
   end
 end
--- test/client_test.rb
+++ test/client_test.rb
@@ -1,11 +1,30 @@
 require "test_helper"
 
 class ClientTest < Minitest::Test
+  attr_reader :client
+
   def setup
-    @client = Mtgapi::Client.new
+    @client = Mtgapi::Client.new("testtoken")
   end
 
-  def test_search_returns_array_of_results
-    assert @client.search("Shock").kind_of? Array
+  def test_id_url
+    assert_equal "id/1?token=#{client.token}", client.id_url(1)
+  end
+
+  def test_search_url
+    assert_equal "/search/test?token=#{client.token}", client.search_url("test")
+  end
+
+  def test_find_by
+    client.stub(:id_url, true) do
+      assert client.find_by(:id, 1)
+    end
+    assert_equal "id/1?token=#{client.token}", client.find_by(:id, 1)
+  end
+
+  def test_search
+    Mtgapi::Client.stub(:get, ->(arg) { return arg }) do
+      assert_equal client.search_url("test"), client.search("test")
+    end
   end
 end
